54b3151959afd6bd0bcab44c5b9311d3857ca7f2
Tanim <tanimtanzilislam9@gmail.com>
2026-07-17 19:05:57 +0600

    displayed countries from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
+import { Suspense } from 'react';
 import './App.css'
 import Countries from './components/Countries'
 
+const countriesPromise= fetch('https://openapi.programming-hero.com/api/all').then(res=>res.json());
 function App() {
   
   return (
     <>
 
     <h1> React world </h1>
-    <Countries></Countries>
+   <Suspense fallback={<p> Loading ... </p>}>
+
+
+  <Countries countriesPromise={countriesPromise}></Countries>
+
+   </Suspense>
     </>
       )
 }
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { use } from 'react';
+const Countries = ({countriesPromise}) => {
 
-const Countries = () => {
+  const countriesData=use(countriesPromise);
+  const countries=countriesData.countries;
+  console.log(countries);
   return (
     <div>
-      
+<h2>  In the countries:{countries.length}</h2>
     </div>
   )
 }
