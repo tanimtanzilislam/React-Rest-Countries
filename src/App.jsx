3ee9cee678cfcb9264129b5fbded298062aa1d0d
Tanim <tanimtanzilislam9@gmail.com>
2026-07-17 17:16:19 +0600

    project setUp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './components/Countries'
+
+function App() {
+  
+  return (
+    <>
+
+    <h1> React world </h1>
+    <Countries></Countries>
+    </>
+      )
+}
+
+export default App
